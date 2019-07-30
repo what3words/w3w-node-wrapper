d8d44fdf3178603dd2c7d947f92510f2998bde76
Tarmo Rohelsaar <tarmo.rohelsaar@gmail.com>
2019-07-30 16:55:17 +0100

    * add peerDependency * add create version file to npm scripts

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,8 +1,21 @@
 import { ErrorResponse } from "./types";
 import { GLOBAL_OPTIONS, searchParams } from "./utils";
-import axios, { AxiosError } from 'axios';
+import axios, { AxiosError } from "axios";
 import { version } from "./version";
 import * as os from 'os';
+
+const platform = (platform => {
+  switch(platform) {
+    case 'darwin':
+      return 'Mac OS X';
+    case 'win32':
+      return 'Windows';
+    case 'linux':
+      return 'Linux';
+    default:
+      return "";
+  }
+})(os.platform());
 
 export const fetchGet = <T>(
   url: string,
@@ -17,24 +30,14 @@ export const fetchGet = <T>(
       data["key"] = GLOBAL_OPTIONS.key;
     }
 
-    var platform = (function(platform) {  
-      switch(platform) {
-        case 'darwin':
-          return 'Mac OS X';
-        case 'win32':
-          return 'Windows';
-        case 'linux':
-          return 'Linux';
-      }
-    })(os.platform());
-
-    axios.request({
+    axios.request<T>({
         url: `${GLOBAL_OPTIONS.baseUrl}/${url}?${searchParams(data)}`,
         headers: {'X-W3W-Wrapper': `what3words-Node/${version} (Node ${process.version}; ${platform} ${os.release()})`}
       })
       .then((response) => {
         if (response && response.status) {
           if (response.status >= 400) {
+            // @ts-ignore
             error((response.data as ErrorResponse).error);
           }
         }
