f558e0e71558b0d32f2902b2ca54246d4db1be06
Matthew <matthewhadwen@gmail.com>
2020-09-30 09:51:09 +0100

    remove export of getOptions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { setOptions, getOptions } from "./utils";
+export { setOptions } from "./utils";
 export { availableLanguages } from "./requests/available-languages";
 export { autosuggest } from "./requests/autosuggest";
 export { convertTo3wa, convertTo3waGeoJson } from "./requests/convert-to-3wa";
