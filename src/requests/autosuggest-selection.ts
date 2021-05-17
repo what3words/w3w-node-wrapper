c53550296117fbf5a82d695cd86c8785bf464554
Christian Shaw <cxs.shaw@googlemail.com>
2021-05-17 11:44:12 +0100

    chore(only call autosuggest-selection for w3w whitelisted domains)

--- src/requests/autosuggest-selection.ts
+++ src/requests/autosuggest-selection.ts
@@ -1,4 +1,5 @@
 import { fetchGet } from '../fetch'
+import { getOptions } from '../utils';
 import { AutosuggestOptions, autosuggestOptionsToQuery } from './autosuggest'
 
 export const autosuggestSelection = (
@@ -8,6 +9,9 @@ export const autosuggestSelection = (
   options: AutosuggestOptions = {},
   sourceApi: 'text' | 'voice' = 'text',
 ): Promise<null> => {
+  const W3W_DOMAIN_REGEX = /^[\w.-_]*.(what3words.com|w3w.io){1}/ig
+  const baseUrl = getOptions().baseUrl
+  if (baseUrl && !W3W_DOMAIN_REGEX.test(baseUrl)) return Promise.resolve(null)
   return fetchGet("autosuggest-selection", {
     'raw-input': rawInput,
     selection,
