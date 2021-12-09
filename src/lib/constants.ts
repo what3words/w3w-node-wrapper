6fb734a89baf44acffa5abc889e19a1b082dd83e
Christian Shaw <cxs.shaw@googlemail.com>
2021-12-09 10:50:42 +0000

    fix linting issues

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,7 @@ import { getPlatform } from './serializer';
 
 export const W3W_REGEX =
   /[^0-9`~!@#$%^&*()+\-_=[{\]}\\|'<,.>?/";:£§º©®\s]{1,}[.｡。･・︒។։။۔።।][^0-9`~!@#$%^&*()+\-_=[{\]}\\|'<,.>?/";:£§º©®\s]{1,}[.｡。･・︒។։။۔።।][^0-9`~!@#$%^&*()+\-_=[{\]}\\|'<,.>?/";:£§º©®\s]{1,}/gi;
-export const VERSION = "__VERSION__";
+export const VERSION = '__VERSION__';
 export const HEADERS = {
   'X-W3W-Wrapper':
     typeof process === 'undefined'
