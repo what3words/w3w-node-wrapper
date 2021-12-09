6e25caf8a79159b2bb24d0e026a8ed23bd8dc360
Christian Shaw <cxs.shaw@googlemail.com>
2021-12-09 10:46:48 +0000

    version bump and version injection without pkginfo

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,11 +1,9 @@
 import { platform, release } from 'os';
 import { getPlatform } from './serializer';
 
-require('pkginfo')(module);
-
 export const W3W_REGEX =
   /[^0-9`~!@#$%^&*()+\-_=[{\]}\\|'<,.>?/";:£§º©®\s]{1,}[.｡。･・︒។։။۔።।][^0-9`~!@#$%^&*()+\-_=[{\]}\\|'<,.>?/";:£§º©®\s]{1,}[.｡。･・︒។։။۔።।][^0-9`~!@#$%^&*()+\-_=[{\]}\\|'<,.>?/";:£§º©®\s]{1,}/gi;
-export const VERSION = module.exports.version;
+export const VERSION = "__VERSION__";
 export const HEADERS = {
   'X-W3W-Wrapper':
     typeof process === 'undefined'
