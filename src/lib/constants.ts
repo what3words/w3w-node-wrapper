ae44f9b618d48452ce276179e8b01779776e98f2
Guillaume Hermet <guillaume.hermet13@gmail.com>
2022-04-20 09:55:10 +0100

    refactor(constants): remove x-w3w-wrapper header

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,14 +1,4 @@
-import { platform, release } from 'os';
-import { getPlatform } from './serializer';
-
 export const W3W_REGEX =
   /[^0-9`~!@#$%^&*()+\-_=[{\]}\\|'<,.>?/";:£§º©®\s]{1,}[.｡。･・︒។։။۔።।][^0-9`~!@#$%^&*()+\-_=[{\]}\\|'<,.>?/";:£§º©®\s]{1,}[.｡。･・︒។։။۔።।][^0-9`~!@#$%^&*()+\-_=[{\]}\\|'<,.>?/";:£§º©®\s]{1,}/gi;
 export const VERSION = '__VERSION__';
-export const HEADERS = {
-  'X-W3W-Wrapper':
-    typeof process === 'undefined'
-      ? `what3words-JavaScript/${VERSION} (${window.navigator.userAgent})`
-      : `what3words-Node/${VERSION} (Node ${process.version}; ${getPlatform(
-          platform()
-        )} ${release()})`,
-};
+export const HEADERS = {};
