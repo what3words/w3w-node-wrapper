9e58d1e251e4220be91f2a56c4ca0c8fe3881832
imndaiga <w.ndaiga@gmail.com>
2024-01-11 12:22:50 +0000

    fix: add what3words api calls

--- docs/examples/vanilla/autosuggest/src/what3words.js
+++ docs/examples/vanilla/autosuggest/src/what3words.js
@@ -5,34 +5,110 @@ import what3words, {
 } from '@what3words/api';
 
 // SETUP
-const W3W_API_KEY = 'TSTSTSTS'; //TODO: Add your what3words API key here
 const COMPONENT_SELECTOR = 'div#w3w-autosuggest';
-const SUGGESTIONS_SELECTOR = 'div#suggestions';
 const INPUT_SELECTOR = 'input#autosuggest';
+const SUGGESTIONS_SELECTOR = 'div#suggestions';
+const W3W_API_KEY = ''; //TODO: Add your what3words API key here
+
+// DECLARATIONS
+let component, input, selected;
+
+/**
+ * Retrieves suggestions from the what3words API given a search string
+ */
+async function getSuggestions(search) {
+  /**
+   * @see {@link https://developer.what3words.com/tutorial/nodejs#usage|Node/Javascript Wrapper}
+   */
+  const { suggestions } = await window.what3words
+    .autosuggest({
+      input: search,
+    })
+    .catch(err => {
+      // you should do something with this
+      console.error(err);
+
+      // Return no suggestions
+      return { suggestions: [] };
+    });
+
+  return suggestions;
+}
+
+/**
+ * Clears the suggestions and removes the elements from the DOM.
+ * TODO - this should probably tear down and remove event listeners that have been attached.
+ */
+function clearSuggestions() {
+  const div = document.createElement('div');
+  document.querySelector(SUGGESTIONS_SELECTOR).remove();
+  div.id = 'suggestions';
+  component.append(div);
+}
+
+/**
+ * Handler for when a suggestion is selected
+ */
+async function selectedSuggestion(suggestion) {
+  // Inform our API which suggestion was selected.
+  await window.what3words.autosuggestSelection(suggestion);
+  // Update input with selected suggestion
+  input.value = `///${suggestion.words}`;
+  selected = suggestion;
+  console.log(`Three word address selected: ${JSON.stringify(selected)}`);
+  // Clear suggestions as a selection has been made
+  clearSuggestions();
+}
+
+/**
+ * Handler for when suggestions are returned from the API
+ */
+function displaySuggestions(suggestions = []) {
+  const parent = document.querySelector(SUGGESTIONS_SELECTOR);
+  const div = document.createElement('div');
+
+  for (const suggestion of suggestions) {
+    const s = document.createElement('div');
+    const address = document.createElement('div');
+    const nearest = document.createElement('div');
+    const hr = document.createElement('hr');
+    address.innerHTML = suggestion.words;
+    nearest.innerHTML = `<small>${suggestion.nearestPlace}</small>`;
+    s.append(address, nearest, hr);
+
+    // Add an event listener when the suggestion is selected
+    s.addEventListener('click', async () => {
+      await selectedSuggestion(suggestion);
+    });
+
+    // Append elements to DOM
+    div.append(s);
+  }
+  if (parent) parent.append(div);
+}
 
 /**
  * This script should be run after the w3w script tags have loaded
  */
 (function loader() {
-  const INPUT = document.querySelector(INPUT_SELECTOR);
-  const W3W = document.querySelector(COMPONENT_SELECTOR);
+  input = document.querySelector(INPUT_SELECTOR);
+  component = document.querySelector(COMPONENT_SELECTOR);
 
   /**
    * what3words is now available!
    */
-  const w3w = what3words(
+  window.what3words = what3words(
     '',
     { apiVersion: ApiVersion.Version3 },
     { transport: axiosTransport() }
   );
   let timeout = null;
-  let selected = null;
 
   // Configure what3words node wrapper
-  w3w.setApiKey(W3W_API_KEY);
+  window.what3words.setApiKey(W3W_API_KEY);
 
   // Attach event listeners to target input element
-  INPUT.addEventListener('focus', ({ target }) => {
+  input.addEventListener('focus', ({ target }) => {
     const { value } = target;
     if (!value) {
       target.value = '///';
@@ -43,7 +119,7 @@ const INPUT_SELECTOR = 'input#autosuggest';
       );
     }
   });
-  INPUT.addEventListener('input', ({ target }) => {
+  input.addEventListener('input', ({ target }) => {
     const { value } = target;
     // Check if the search string is a 3wa-like string - if not do nothing.
     if (!W3W_REGEX.test(value)) return;
@@ -55,90 +131,16 @@ const INPUT_SELECTOR = 'input#autosuggest';
       displaySuggestions(suggestions);
     }, 300);
   });
-  INPUT.addEventListener('blur', event => {
+  input.addEventListener('blur', event => {
     const {
       target: { value },
     } = event;
     event.stopPropagation();
     setTimeout(() => {
       if (!W3W_REGEX.test(value)) {
-        INPUT.value = '';
+        input.value = '';
       }
       clearSuggestions();
     }, 300);
   });
-
-  /**
-   * Retrieves suggestions from the what3words API given a search string
-   */
-  async function getSuggestions(search) {
-    /**
-     * @see {@link https://developer.what3words.com/tutorial/nodejs#usage|Node/Javascript Wrapper}
-     */
-    const { suggestions } = await w3w
-      .autosuggest({
-        input: search,
-      })
-      .catch(err => {
-        // you should do something with this
-        console.error(err);
-
-        // Return no suggestions
-        return { suggestions: [] };
-      });
-
-    return suggestions;
-  }
-
-  /**
-   * Clears the suggestions and removes the elements from the DOM.
-   * TODO - this should probably tear down and remove event listeners that have been attached.
-   */
-  function clearSuggestions() {
-    const div = document.createElement('div');
-    document.querySelector(SUGGESTIONS_SELECTOR).remove();
-    div.id = 'suggestions';
-    W3W.append(div);
-  }
-
-  /**
-   * Handler for when a suggestion is selected
-   */
-  async function selectedSuggestion(suggestion) {
-    // Inform our API which suggestion was selected.
-    await w3w.autosuggestSelection(suggestion);
-    // Update input with selected suggestion
-    INPUT.value = `///${suggestion.words}`;
-    selected = suggestion;
-    console.log(`Three word address selected: ${JSON.stringify(selected)}`);
-    // Clear suggestions as a selection has been made
-    clearSuggestions();
-  }
-
-  /**
-   * Handler for when suggestions are returned from the API
-   */
-  function displaySuggestions(suggestions = []) {
-    const parent = document.querySelector(SUGGESTIONS_SELECTOR);
-    const div = document.createElement('div');
-
-    for (const suggestion of suggestions) {
-      const s = document.createElement('div');
-      const address = document.createElement('div');
-      const nearest = document.createElement('div');
-      const hr = document.createElement('hr');
-      address.innerHTML = suggestion.words;
-      nearest.innerHTML = `<small>${suggestion.nearestPlace}</small>`;
-      s.append(address, nearest, hr);
-
-      // Add an event listener when the suggestion is selected
-      s.addEventListener('click', async () => {
-        await selectedSuggestion(suggestion);
-      });
-
-      // Append elements to DOM
-      div.append(s);
-    }
-    if (parent) parent.append(div);
-  }
 })();
--- docs/examples/vanilla/map/src/what3words.js
+++ docs/examples/vanilla/map/src/what3words.js
@@ -1,16 +1,42 @@
 import what3words, { axiosTransport, ApiVersion } from '@what3words/api';
 import { initMap } from './google';
 
+const DEFAULT_MAP_CENTER = { lat: 51.52086, lng: -0.195499 };
+
 // SETUP
-const W3W_API_KEY = 'TSTSTSTS'; //TODO: Add your what3words API key here
 const MAP_SELECTOR = 'div#w3w-map';
-const MAP_CENTER = { lat: 51.52086, lng: -0.195499 };
 const MAP_ZOOM = 20;
+const THREE_WORD_ADDRESS = 'filled.count.soap';
+const W3W_API_KEY = ''; //TODO: Add your what3words API key here
 
 // DECLARATIONS
-window.what3words = null;
-let grid = [];
-let map = null;
+let address,
+  div,
+  input,
+  grid = [],
+  languages,
+  map,
+  mapCenter;
+
+function getControls() {
+  div = document.createElement('div');
+  input = document.createElement('input');
+
+  // Add element styling
+  div.style.margin = '50px';
+  div.style.display = 'flex';
+  div.style.flexDirection = 'column';
+  div.style.flexGrow = 1;
+  input.style.lineHeight = '25px';
+  input.style.minWidth = '250px';
+
+  // Add input attributes
+  input.type = 'text';
+  input.index = 1;
+
+  div.appendChild(input);
+  return div;
+}
 
 function clearGrid() {
   if (grid) grid.forEach(g => g.setMap(null));
@@ -34,6 +60,34 @@ function getGrid() {
       },
     },
   });
+}
+
+function onMapClick(e) {
+  const latLng = e.latLng.toJSON();
+  // Convert a coordinate to a three word address
+  window.what3words.convertTo3wa({ coordinates: latLng }).then(res => {
+    plot3wa(res);
+    if (map.getZoom() < 20) {
+      map.panTo(res.coordinates);
+      map.setZoom(20);
+    }
+  });
+}
+
+function plot3wa(new3wa) {
+  if (!new3wa) return;
+  const { square, words } = new3wa;
+  if (address) address.setMap(null);
+
+  address = new google.maps.Rectangle({
+    bounds: new google.maps.LatLngBounds(square.southwest, square.northeast),
+    strokeWeight: 1,
+    fillColor: '#0A3049',
+    strokeColor: '#0A3049',
+    strokePosition: google.maps.StrokePosition.INSIDE,
+  });
+  input.value = '///' + words;
+  address.setMap(map);
 }
 
 function plotGrid(newGrid) {
@@ -68,15 +122,35 @@ function plotGrid(newGrid) {
     { apiVersion: ApiVersion.Version3 },
     { transport: axiosTransport() }
   );
-  map = initMap({
-    selector: MAP_SELECTOR,
-    center: MAP_CENTER,
-    zoom: MAP_ZOOM,
+
+  // Retrieve and print out supported languages
+  window.what3words.availableLanguages().then(res => {
+    languages = res.languages;
+    console.log({ supportedLanguages: languages });
   });
 
-  map.addListener('bounds_changed', e => {
-    const zoom = map.getZoom();
-    clearGrid();
-    if (zoom > 15) getGrid(map).then(plotGrid);
-  });
+  // Retrieve coordinates for initial three word address
+  window.what3words
+    .convertToCoordinates({ words: THREE_WORD_ADDRESS })
+    .then(res => {
+      mapCenter = res.coordinates;
+      console.log(res);
+    })
+    .finally(() => {
+      // Initialise map instance
+      map = initMap({
+        selector: MAP_SELECTOR,
+        center: mapCenter || DEFAULT_MAP_CENTER,
+        zoom: MAP_ZOOM,
+      });
+
+      const controls = getControls();
+      map.controls[google.maps.ControlPosition.TOP_CENTER].push(controls);
+      map.addListener('bounds_changed', e => {
+        const zoom = map.getZoom();
+        clearGrid();
+        if (zoom > 15) getGrid(map).then(plotGrid);
+      });
+      map.addListener('click', onMapClick);
+    });
 })();
