f55cd6525dfacc96f36a18c6971a42c6f917c56e
Frederick Lee <frederickrichard.lee@gmail.com>
2024-03-05 15:14:36 +0000

    tests: using chancejs for locale

--- test/lib/languages.spec.ts
+++ test/lib/languages.spec.ts
@@ -3,25 +3,17 @@ import { Chance } from 'chance';
 import { baseLanguageCodeForISO6391 } from '../../src';
 
 const CHANCE = new Chance();
-const ISO6391_LANGUAGES = [
-  'it-IT',
-  'en-GB',
-  'zh-CN',
-  'pt-BR',
-  'es-ES',
-  'fr-FR',
-  'de-DE',
-];
+
 describe('baseLanguageCodeForISO6391', () => {
   it('should return base language code for valid ISO 639-1 language code', () => {
-    const languageCode = CHANCE.pickone(ISO6391_LANGUAGES);
+    const languageCode = CHANCE.locale({ region: true });
     baseLanguageCodeForISO6391(languageCode)?.should.be.equal(
       languageCode.substring(0, 2)
     );
   });
 
   it('should allow case insensitive language code', () => {
-    const languageCode = CHANCE.pickone(ISO6391_LANGUAGES);
+    const languageCode = CHANCE.locale({ region: true });
     baseLanguageCodeForISO6391(languageCode.toUpperCase())?.should.be.equal(
       languageCode.substring(0, 2)
     );
