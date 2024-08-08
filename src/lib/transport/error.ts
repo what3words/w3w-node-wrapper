11797c561cd568f0c5e518126589d05121ecf8f4
Frederick Lee <chaddgrimm@users.noreply.github.com>
2024-08-08 19:09:48 +0800

    [TT-9585] Added 402 error handling (#74)
    * chore: sl language code added
    
    * chore: added 402 error
    
    * chore: added details property on transport error
    
    * chore: minor refactor
    
    * chore: added body to error handler for axios
    
    * tests: added tests for 402 error
    
    * chore: minor clean up

--- src/lib/transport/error.ts
+++ src/lib/transport/error.ts
@@ -1,48 +1,59 @@
 import type { TransportResponse } from './model';
 
-export class TransportError extends Error {
-  constructor(message: string, public readonly status: number) {
+export class TransportError<T> extends Error {
+  constructor(
+    message: string,
+    public readonly status: number,
+    public readonly details?: T
+  ) {
     super(message);
   }
 }
-export class BadRequestError extends TransportError {
-  constructor(message?: string) {
-    super(message || 'Bad Request', 400);
+export class BadRequestError<T> extends TransportError<T> {
+  constructor(message?: string, details?: T) {
+    super(message || 'Bad Request', 400, details);
   }
 }
-export class UnauthorizedError extends TransportError {
-  constructor(message?: string) {
-    super(message || 'Unauthorized', 401);
+export class UnauthorizedError<T> extends TransportError<T> {
+  constructor(message?: string, details?: T) {
+    super(message || 'Unauthorized', 401, details);
   }
 }
-export class ForbiddenError extends TransportError {
-  constructor(message?: string) {
-    super(message || 'Forbidden', 403);
+
+export class PaymentRequiredError<T> extends TransportError<T> {
+  constructor(message?: string, details?: T) {
+    super(message || 'Payment Required', 402, details);
   }
 }
-export class NotFoundError extends TransportError {
-  constructor(message?: string) {
-    super(message || 'Not Found', 404);
+
+export class ForbiddenError<T> extends TransportError<T> {
+  constructor(message?: string, details?: T) {
+    super(message || 'Forbidden', 403, details);
   }
 }
-export class InternalServerError extends TransportError {
-  constructor(message?: string) {
-    super(message || 'Internal Server Error', 500);
+export class NotFoundError<T> extends TransportError<T> {
+  constructor(message?: string, details?: T) {
+    super(message || 'Not Found', 404, details);
   }
 }
-export class BadGatewayError extends TransportError {
-  constructor(message?: string) {
-    super(message || 'Bad Gateway', 502);
+export class InternalServerError<T> extends TransportError<T> {
+  constructor(message?: string, details?: T) {
+    super(message || 'Internal Server Error', 500, details);
   }
 }
-export class ServiceUnavailableError extends TransportError {
-  constructor(message?: string) {
-    super(message || 'Service Unavailable', 503);
+export class BadGatewayError<T> extends TransportError<T> {
+  constructor(message?: string, details?: T) {
+    super(message || 'Bad Gateway', 502, details);
   }
 }
-export class GatewayTimeoutError extends TransportError {
-  constructor(message?: string) {
-    super(message || 'Gateway Timeout', 504);
+export class ServiceUnavailableError<T> extends TransportError<T> {
+  constructor(message?: string, details?: T) {
+    super(message || 'Service Unavailable', 503, details);
+  }
+}
+export class GatewayTimeoutError<T> extends TransportError<T> {
+  constructor(message?: string, details?: T) {
+    super(message || 'Gateway Timeout', 504, details);
   }
 }
 
@@ -50,27 +61,31 @@ export function errorHandler<T>(
   response: TransportResponse<T>
 ): TransportResponse<T> {
   if (!response) return { status: 204, statusText: 'No Content' };
-  const { status, statusText: message } = response;
+  const { status, statusText: message, body } = response;
+  const details =
+    body && typeof body === 'object' && 'error' in body ? body.error : body;
   if (status >= 400)
     switch (status) {
       case 400:
-        throw new BadRequestError(message);
+        throw new BadRequestError(message, details);
       case 401:
-        throw new UnauthorizedError(message);
+        throw new UnauthorizedError(message, details);
+      case 402:
+        throw new PaymentRequiredError(message, details);
       case 403:
-        throw new ForbiddenError(message);
+        throw new ForbiddenError(message, details);
       case 404:
-        throw new NotFoundError(message);
+        throw new NotFoundError(message, details);
       case 500:
-        throw new InternalServerError(message);
+        throw new InternalServerError(message, details);
       case 502:
-        throw new BadGatewayError(message);
+        throw new BadGatewayError(message, details);
       case 503:
-        throw new ServiceUnavailableError(message);
+        throw new ServiceUnavailableError(message, details);
       case 504:
-        throw new GatewayTimeoutError(message);
+        throw new GatewayTimeoutError(message, details);
       default:
-        throw new TransportError(message || 'Transport Error', status);
+        throw new TransportError(message || 'Transport Error', status, details);
     }
   return response;
 }
